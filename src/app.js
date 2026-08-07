9cb1ad7854814e007e5f18446c072ff597318a58
Tejakashina <kashinatejasri22@gmail.com>
2026-08-07 23:23:48 +0530

    playing with params, query

--- src/app.js
+++ src/app.js
@@ -1,17 +1,11 @@
 const express = require('express')
 const app = express()
-
-app.use('/user', (req, res) => {
-    res.send("Request received from user")
-}) //will match all http methods api calls to /user
-app.get('/user', (req, res) => {
+app.get('/user/:userId/:age', (req, res) => {
+    console.log(req.params)
     res.send({"firstName": "Teja", "lastName": "Kiran"})
 })
-app.post('/user', (req, res) => {
-    res.send("User Added successfully")
-})
-app.delete('/user', (req, res) => {
-    res.send("User Deleted Successfully")
+app.get(/^\/ab*c$/, (req, res) => {
+    res.send({"firstName": "kiran", "lastName": "payyavula"})
 })
 app.listen(7777, () => {
     console.log("Server is running on port 7777")
