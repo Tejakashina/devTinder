abe7803f567288b42059dfb1f5c999a779d2e039
Tejakashina <kashinatejasri22@gmail.com>
2026-08-07 20:07:23 +0530

    Exploring get, post, delete

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const app = express()
-app.use('/test', (req, res) => {
-    res.send("Request received from test")
-})
-app.use('/',(req, res) => {
-    res.send("Hello Teja")
-})
 
+app.use('/user', (req, res) => {
+    res.send("Request received from user")
+}) //will match all http methods api calls to /user
+app.get('/user', (req, res) => {
+    res.send({"firstName": "Teja", "lastName": "Kiran"})
+})
+app.post('/user', (req, res) => {
+    res.send("User Added successfully")
+})
+app.delete('/user', (req, res) => {
+    res.send("User Deleted Successfully")
+})
 app.listen(7777, () => {
     console.log("Server is running on port 7777")
 })
