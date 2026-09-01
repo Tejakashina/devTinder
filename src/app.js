28a69416be095a158a7f4f145bc7ed5cf014f644
Tejakashina <kashinatejasri22@gmail.com>
2026-09-01 21:31:49 +0530

    moved dotenv to staring line

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const app = express()
 const connectDB = require('./config/database')
 const cookieParser = require('cookie-parser')
 const PORT = process.env.PORT || 7777
-require('dotenv').config()
 require('./utils/cronjob')
 app.use(cors({
     origin: 'http://localhost:5173',
